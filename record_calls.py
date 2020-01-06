8823b112be95d24f7b1643121467fcd96c7d1e0f
Paul Wilson <paul.wilson@suez.com>
2020-01-07 07:48:49 +1100

    updating from laptop

--- record_calls.py
+++ record_calls.py
@@ -1,12 +1,19 @@
+from functools import wraps
+from dataclasses import dataclass
+
+@dataclass
+class Call:
+    args: tuple
+    kwargs: dict
+
 def record_calls(func):
-    def wrapper(*args):
-        try:
-            func.call_count += 1
-        except AttributeError:
-            func.call_count = 1
-        print(f'Running {func.__name__}')
-        print(func.call_count)
-        return func(*args)
+    @wraps(func)
+    def wrapper(*args ,**kwargs):
+        wrapper.call_count += 1
+        wrapper.calls.append(Call(args, kwargs))
+        return func(*args, **kwargs)
+    wrapper.call_count = 0
+    wrapper.calls = []
     return wrapper
 
 @record_calls
